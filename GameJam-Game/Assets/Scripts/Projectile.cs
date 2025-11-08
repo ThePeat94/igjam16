0c1501f8110e1c1b02c96f9c93f76784e7c45ec3
Hendrik Rakemann <dev@hendrikrakemann.de>
2025-11-08 13:18:17 +0100

    enemy shooter and player health data

---
+++ GameJam-Game/Assets/Scripts/Projectile.cs
@@ -0,0 +1,31 @@
+using System;
+using UnityEngine;
+
+namespace Nidavellir
+{
+    public class Projectile : MonoBehaviour
+    {
+        [SerializeField] private float m_damage;
+        [SerializeField] private Rigidbody2D m_rigidbody;
+        
+        public Vector2 Velocity { get; set; }
+
+        private void Awake()
+        {
+            this.m_rigidbody ??= this.GetComponent<Rigidbody2D>();
+        }
+
+        private void FixedUpdate()
+        {
+            this.m_rigidbody.linearVelocity = this.Velocity;
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!other.GetComponent<PlayerController>()) 
+                return;
+            var healthController = other.GetComponent<HealthController>();
+            healthController?.ProcessDamage(this.m_damage);
+        }
+    }
+}
